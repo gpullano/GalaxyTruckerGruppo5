4187eb866e88a3418ed5eb390e8471ef12cfa05a
ValerioTolotti <v.tolotti@studenti.unibg.it>
2025-04-28 11:28:20 +0200

    Ho creato la classe 'SupportoVitaleViola'

--- src/tessere/SupportoVitaleViola.java
+++ src/tessere/SupportoVitaleViola.java
@@ -3,7 +3,7 @@ package tessere;
 public class SupportoVitaleViola extends Tessera {
 	// attributo unico che è il colore
 	private final String colore;
-	
+	// costruttore ereditato dalla superclasse 'Tessera'
 	public SupportoVitaleViola(Connettore latoDx, Connettore latoSx, Connettore latoSup, Connettore latoDown,String colore) {
 		super(latoDx, latoSx, latoSup, latoDown);
 		this.colore=colore;
