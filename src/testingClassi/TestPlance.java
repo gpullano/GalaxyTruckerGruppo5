55343b83677152a82050681532c9db12fa7e5b32
giorg <giorg@BOOK-VDRKJP4KG7.homenet.telecomitalia.it>
2025-05-11 19:17:23 +0200

    Ho implementato la plancia volo

--- src/testingClassi/TestPlance.java
+++ src/testingClassi/TestPlance.java
@@ -1,11 +1,16 @@
 package testingClassi;
 
 import plance.PlanceNaveLivello1;
+import plance.PlanceVolo;
 
 public class TestPlance {
 	public static void main(String[] args) {
 		PlanceNaveLivello1 s = new PlanceNaveLivello1();
 		s.stampaNave();
+		
+		PlanceVolo p = new PlanceVolo(5, 8);
+		p.percorso();
+		p.stampaVolo();
 	}
 
 }
