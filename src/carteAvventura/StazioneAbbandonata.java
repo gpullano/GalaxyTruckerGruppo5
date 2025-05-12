1058c77e7948277acfd4a89888063758fb2bb1b5
ValerioTolotti <v.tolotti@studenti.unibg.it>
2025-05-12 10:08:05 +0200

    Ho implementato le classi "Merci" e "Stazione abbandonata"

---
+++ src/carteAvventura/StazioneAbbandonata.java
@@ -0,0 +1,35 @@
+package carteAvventura;
+import collezionabili.Merci;
+import java.util.Random;
+
+public class StazioneAbbandonata extends CartaPerditaGiorniVolo {
+		// attributi 
+	private final int equipaggioRichiesto;
+	private final Merci merce;
+	
+	
+	public StazioneAbbandonata(int livello) {
+		super(livello);
+		Random rand=new Random();
+		this.equipaggioRichiesto=rand.nextInt(3)+4;
+		this.merce=Merci.generaMerceCasuale();
+	}
+	
+	 
+	@Override
+	public void attiva() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	public int getEquipaggioRichiesto() {
+		return equipaggioRichiesto;
+	}
+
+
+	public Merci getMerce() {
+		return merce;
+	}
+
+}
--- src/collezionabili/Merci.java
+++ src/collezionabili/Merci.java
@@ -1,8 +1,15 @@
 package collezionabili;
-
+import java.util.Random;
 public enum Merci {
 	BLU(1), VERDE(2), GIALLA(3), ROSSA(4);
 
 	Merci(int i) {
-	}
+		}
+	// generatore di merci casuale
+	 public static Merci generaMerceCasuale() {
+		 Random rand=new Random();
+	        Merci[] valori = Merci.values();
+	        int indice = rand.nextInt(valori.length);
+	        return valori[indice];
+	    }
 }
