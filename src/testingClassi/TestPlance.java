383d8feea62eea7803f446a1bf3bb6c737d7f4a4
giorg <giorg@BOOK-VDRKJP4KG7.homenet.telecomitalia.it>
2025-05-17 11:47:34 +0200

    Ho modificato la classe test plance

--- src/testingClassi/TestPlance.java
+++ src/testingClassi/TestPlance.java
@@ -1,5 +1,6 @@
 package testingClassi;
 
+import carteAvventura.CartaPianeti;
 import plance.PlanceNaveLivello1;
 import plance.PlanceVolo;
 
@@ -11,6 +12,9 @@ public class TestPlance {
 		PlanceVolo p = new PlanceVolo(5, 8);
 		p.percorso();
 		p.stampaVolo();
+		
+		CartaPianeti carta = new CartaPianeti(1);
+		System.out.println(carta.toString());
 	}
 
 }
