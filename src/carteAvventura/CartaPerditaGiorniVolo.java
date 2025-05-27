3fa793ae9701f44ee7d40d48fbff95ca4e319aef
saidfatma <f.said@studenti.unibg.it>
2025-05-27 21:03:19 +0200

    ho implimentato the attiva method of pioggiaDiMeteoriti with adding two method in planceNave

--- src/carteAvventura/CartaPerditaGiorniVolo.java
+++ src/carteAvventura/CartaPerditaGiorniVolo.java
@@ -2,12 +2,13 @@ package carteAvventura;
 
 import java.util.Random;
 
+
 public abstract class CartaPerditaGiorniVolo extends Carta{
 	//attributi
 	private final int giorniVoloPersi;
 	
 	//costruttore
-	protected CartaPerditaGiorniVolo(int livello) {
+	public CartaPerditaGiorniVolo(int livello) {
 		super(livello);
 		this.giorniVoloPersi = new Random().nextInt(3) + 1;
 	}
