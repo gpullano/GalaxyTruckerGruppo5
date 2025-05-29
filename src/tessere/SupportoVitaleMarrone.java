12bd2702ef5726887d60fbfb0af3ec5d1c869b2a
saraounifi <s.ounifi@studenti.unibg.it>
2025-05-29 21:08:21 +0200

    Update SupportoVitaleMarrone.java
    JD

--- src/tessere/SupportoVitaleMarrone.java
+++ src/tessere/SupportoVitaleMarrone.java
@@ -1,16 +1,38 @@
 package tessere;
 
+/** 
+*rappresenta un supporto vitale per equipaggio marrone.
+*/
 public class SupportoVitaleMarrone extends Tessera {
 	// attributo unico che è il colore 
 	private final String colore;
 	// costruttore ereditato dalla superclasse 'Tessera'
+	
+/**
+*costruttore supportovitalemarrone
+*@param latoDx connettori lato destro
+*@param latoSx connettori lato sinistro
+*@param latoSup connettori lato superiori
+*@param latoDown connettori lato inferiori
+*@param colore   colore associato
+*/
 	public SupportoVitaleMarrone(Connettore latoDx, Connettore latoSx, Connettore latoSup, Connettore latoDown,String colore) {
 		super(latoDx, latoSx, latoSup, latoDown);
 		this.colore=colore;
 	}
+
+/**
+*retituisce il colore assocciato al supporto vitale. 
+*@retuurn colore stringa
+*/
 	public String getColore() {
 		return colore;
-	}
+}
+
+/**
+*retituisce il nome breve tessera
+*@return stringa STVM
+*/
 	@Override
 	public String getNomeBreve() {
     	return " STVM ";
