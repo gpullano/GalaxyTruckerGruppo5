8f006518253d4b111260b3a28e41913216c9717e
saraounifi <s.ounifi@studenti.unibg.it>
2025-05-29 18:26:37 +0200

    Update CabinaCentrale.java
    javadoc

--- src/tessere/CabinaCentrale.java
+++ src/tessere/CabinaCentrale.java
@@ -2,13 +2,22 @@ package tessere;
 
 import gameLogic.Colore;
 
-public class CabinaCentrale extends Tessera {
+/**
+* La classe CabinaCentrale rappresenta una tessera centrale della nave.
+*contiene due membri dell'equipaggio iniziale ed è associata ad un colore specifico.
+*/
+public class CabinaCentrale exetends Tessera {
 	// attributi 
 	private final Colore colore;
 	private int equipaggio;
 	// non possiede alieni
-	
 
+
+/**
+*costruttore della cabinacentrale.
+*imposta i connettori universali e imposta 2 membri dell'equipaggio.
+*@param colore il colore assocciato alla cabina centrale.
+*/
 	public CabinaCentrale(Colore colore) {
 		super(Connettore.UNIVERSALE, Connettore.UNIVERSALE, Connettore.UNIVERSALE, Connettore.UNIVERSALE);
 		this.setEquipaggio(2);
@@ -16,20 +25,34 @@ public class CabinaCentrale extends Tessera {
 
 	}
 
-
+/**
+*restituisce il numero di membri dell'equipaggio nella cabina centrale.
+*@return numero membri equipaggio
+*/
 	public int getEquipaggio() {
 		return equipaggio;
 	}
 
-
+/**
+*imposta il numero di membri dell'equipaggio nella cabina centrale.
+*@param equipaggio numero da assegnare
+*/
 	public void setEquipaggio(int equipaggio) {
 		this.equipaggio = equipaggio;
 	}
 
-
-	public Colore getColore() {
+/**
+*restituisce il colore della cabina centrale.
+*@return colore cabina
+*/
+       public Colore getColore() {
 		return colore;
 	}
+
+/**
+*restituisce il nome breve della tessera.
+*@return stringa CabCen
+*/
 	@Override
 	public String getNomeBreve() {
     	return "CabCen";
