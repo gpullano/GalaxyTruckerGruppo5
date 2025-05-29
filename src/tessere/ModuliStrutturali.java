4c422326ddd46e2ec11d14a1a36870e9e0ca12d2
saraounifi <s.ounifi@studenti.unibg.it>
2025-05-29 20:13:00 +0200

    Update ModuliStrutturali.java
    javadoc

--- src/tessere/ModuliStrutturali.java
+++ src/tessere/ModuliStrutturali.java
@@ -1,12 +1,26 @@
 package tessere;
 
+/**
+*rappresenta una tesser per i collegamenti.
+*/
 public class ModuliStrutturali extends Tessera{
 
+/**
+*costruttore moduli strutturali
+*@param latoDx connettori lato destro
+*@param latoSx connettori lato sinistro
+*@param latoSup connettori lato superiori
+*@param latoDown connettori lato inferiori
+*/
 	public ModuliStrutturali(Connettore latoDx, Connettore latoSx, Connettore latoSup, Connettore latoDown) {
 		super(latoDx, latoSx, latoSup, latoDown);
 		
 	}
-	
+
+/**
+*restituisce il nome breve della tessera.
+*@return stringa ModSt
+*/
 	@Override
 	public String getNomeBreve() {
     	return " ModSt ";
