084f15a7a5c0eb89cff598d1056a3250372df00b
saraounifi <s.ounifi@studenti.unibg.it>
2025-05-29 20:42:43 +0200

    Update Merci.java
    javadoc

--- src/collezionabili/Merci.java
+++ src/collezionabili/Merci.java
@@ -2,17 +2,33 @@ package collezionabili;
 
 import gameLogic.Colore;
 
+/**
+*rappresenta una merce con un coloe associat.
+*/
+
 public class Merci {
 	private Colore colore;
-	
+
+/**
+*costruttore della classe merci
+*@param colore il colore della merce
+*/
 	public Merci (Colore colore) {
 		this.setColore(colore);
 	}
 
+/**
+*restituice il colore della merce
+*@return colore associato alla merce
+*/
 	public Colore getColore() {
 		return colore;
 	}
 
+/**
+*imposta il colore della merce.
+*@param colore il nuovo colore asociato alla merce.
+*/
 	public void setColore(Colore colore) {
 		this.colore = colore;
 	}
