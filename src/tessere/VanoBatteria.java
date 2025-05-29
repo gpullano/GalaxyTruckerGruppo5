09be46974d71724dde1eab15086eb3ec094e4e7f
saraounifi <s.ounifi@studenti.unibg.it>
2025-05-29 21:17:27 +0200

    Update VanoBatteria.java
    JD

--- src/tessere/VanoBatteria.java
+++ src/tessere/VanoBatteria.java
@@ -1,17 +1,43 @@
 package tessere;
 
+
+/** 
+*rappresenta una tessera contenente un vano batteria.
+* fornisce energia ad altre tessere
+*/
 public class VanoBatteria extends Tessera {
 	// attributo
 	private int batterie;
+	
+	
+/**
+*costruttore vanobatteria
+*@param latoDx connettori lato destro
+*@param latoSx connettori lato sinistro
+*@param latoSup connettori lato superiori
+*@param latoDown connettori lato inferiori
+*@param batterie  quantità batterie presenti
+*/
 	//costruttore ereditato superclasse
 	public VanoBatteria(Connettore latoDx, Connettore latoSx, Connettore latoSup, Connettore latoDown, int batterie) {
 		super(latoDx, latoSx, latoSup, latoDown);
 		this.batterie=batterie;
 	}
-	//getter 
+
+	
+/**
+*retituisce il numero di batterie nel vano.
+*@return numero batterie
+*/
+//getter 
 	public int getBatterie() {
 		return batterie;
 	}
+
+/**
+*retituisce il nome breve tessera
+*@return stringa VanBa
+*/ 
 	@Override
 	public String getNomeBreve() {
     	return "VanBa";
@@ -21,3 +47,18 @@ public class VanoBatteria extends Tessera {
 	
 	
 }
+
+
+
+
+	
+
+	
+
+
+
+	
+	
+
+
+	
