9d0799cfb91dc999d68a66ab5627cc99563fe4c7
saraounifi <s.ounifi@studenti.unibg.it>
2025-05-29 21:02:08 +0200

    Update Stiva.java
    jd

--- src/tessere/Stiva.java
+++ src/tessere/Stiva.java
@@ -1,9 +1,21 @@
 package tessere;
 
+/**
+*rappresenta una tessera usata per immagazzinare merci.
+*può avere 2 o 3 scomparti
+*/
 public class Stiva extends Tessera {
 	//può avere dai 2 ai 3 scomparti da generare casualmente 
 	private final int scomparti;
+
 	
+/**
+*costruttore stiva
+*@param latoDx connettori lato destro
+*@param latoSx connettori lato sinistro
+*@param latoSup connettori lato superiori
+*@param latoDown connettori lato inferiori
+*/
 	public Stiva(Connettore latoDx, Connettore latoSx, Connettore latoSup, Connettore latoDown,int scomparti) {
 		super(latoDx, latoSx, latoSup, latoDown);
 		this.scomparti=scomparti;
@@ -11,9 +23,18 @@ public class Stiva extends Tessera {
 		
 	}
 
+/** 
+*restituisce il numero di scomparti della stiva.
+*@return numero scomparti
+*/
 	public int getScomparto() {
 		return scomparti;
 	}
+
+/** 
+*restituisce il nome breve della tessera
+*@return stringa stiva
+*/
 	@Override
 	public String getNomeBreve() {
     	return " Stiva ";
