9966a1b7e1cf762b52b6b3fbf1e885e3fc9a4bf4
saraounifi <s.ounifi@studenti.unibg.it>
2025-05-29 17:33:45 +0200

    Update Attivabile.java
    javadoc

--- src/tessere/Attivabile.java
+++ src/tessere/Attivabile.java
@@ -1,7 +1,15 @@
 package tessere;
 
+/**
+*interfaccia che fornisce a determinate tessere il metodo attivabile, da implementare.
+*esempi sono: gli scudi, i cannoni e i motori doppi, le batterie.
+*/
 public interface Attivabile {
-	// interfaccia che fornisce a determinate tessere il metodo attivabile, da implementare.
-	// esempi sono: gli scudi, i cannoni e i motori doppi, le batterie.
+
+/**
+* Definisce l'attivazione della tessera in base all'energia che c'è.
+* @param energia true se la tessera riceve energia se no false.
+*/
+
 	public void attiva(boolean energia);
 }
