b8af579d5cd50f7000a23aec7e27c31e1da8e7c7
saraounifi <s.ounifi@studenti.unibg.it>
2025-05-29 20:06:48 +0200

    Update GeneratoreScudi.java
    javadoc

--- src/tessere/GeneratoreScudi.java
+++ src/tessere/GeneratoreScudi.java
@@ -1,14 +1,34 @@
 package tessere;
 
+/**
+* generatore scudi per proteggere la nave.
+*/
 public class GeneratoreScudi extends Tessera implements Attivabile{ 
+	
+/**
+*costruttore del generatorescudi.
+*@param latoDx connettore lato destro
+*@param latoSx connettore lato sinistro
+*@param latoSup connettore lato superiore
+*@param latoDown connettore lato inferiore
+*/
 	public GeneratoreScudi(Connettore latoDx, Connettore latoSx, Connettore latoSup, Connettore latoDown) {
 		super(latoDx, latoSx, latoSup, latoDown);
 	}
 
+/**
+*attiva il generatore scudi se alimentato.
+*@param energia true se alimentato se no false
+*/
 	@Override
 	public void attiva(boolean energia) {
 		System.out.println("Ti sei difeso con lo scudo!");
 	}
+
+/**
+*restituisce il nome breve della tessera.
+*@return stringa scudo
+*/
 	@Override
 	public String getNomeBreve() {
     	return " Scudo ";
