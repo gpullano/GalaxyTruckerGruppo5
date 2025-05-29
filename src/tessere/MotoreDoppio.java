a6a46db6f2e24edb705fbfdd6e000b88f0764a62
saraounifi <s.ounifi@studenti.unibg.it>
2025-05-29 20:53:50 +0200

    Update MotoreDoppio.java
    JAVADOC

--- src/tessere/MotoreDoppio.java
+++ src/tessere/MotoreDoppio.java
@@ -1,22 +1,49 @@
 package tessere;
 
+/** 
+*rappresenta una tessea che può  essere attivata per dare maggiore potenza
+*/
 public class MotoreDoppio extends Tessera implements Attivabile{
-	// costruttore
+	
+/**
+*costruttore motoredoppio
+*@param latoDx connettori lato destro
+*@param latoSx connettori lato sinistro
+*@param latoSup connettori lato superiori
+*@param latoDown connettori lato inferiori
+*/	
+// costruttore
 	public MotoreDoppio(Connettore latoDx, Connettore latoSx, Connettore latoSup, Connettore latoDown) {
 		super(latoDx, latoSx, latoSup, latoDown);
 	}
-	// getter
+	
+	
+/**
+*restituisce la potenza del motore doppio
+*@return potenza pari a 2
+*/
+// getter
 	public int getPotenza() {
 		return 2;
 	}
-	// metodi 
+	
+/**
+*attiva il motore doppio se viene data energia.
+*@param energia true per attivare il motore se no false
+*/
+// metodi 
 	@Override
 	public void attiva(boolean energia) {
 			if(energia) {
 				getPotenza();
 			}
 	}
-	@Override
+
+/**
+*restituisce il nome breve della tessera
+*@return string MotoD
+*/
+        @Override
 	public String getNomeBreve() {
     	return " MotoD ";
 	}
