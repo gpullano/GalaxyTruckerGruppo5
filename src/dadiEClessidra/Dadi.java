1f8f820caad162636b8759eac71ff9f534ce074a
saraounifi <s.ounifi@studenti.unibg.it>
2025-05-29 17:27:07 +0200

    Update Dadi.java
    Ho aggiunto la javadoc

--- src/dadiEClessidra/Dadi.java
+++ src/dadiEClessidra/Dadi.java
@@ -2,17 +2,29 @@ package dadiEClessidra;
 
 import java.util.Random;
 
+/**
+* La classe Dadi simula un dado con 12 facce. 
+* Si può lanciare per ottenere un valore casuale da 1 a 12.
+*/
 public class Dadi {
     //attribute
     int faccia;
     Random rn = new Random();
 
+/**
+* Costruttore classe Dadi.
+* imposta la faccia a zero.
+*/
     //constructor
     public Dadi(){
         this.faccia=0;
         
     }
-    
+
+/**
+* Simula il lancio del dado.
+* @ return Il valore casuale ottenuto dal lancio tra 1 e 12.
+*/
     //methods
     public int lancia(){         
         this.faccia=rn.nextInt(12)+1;
