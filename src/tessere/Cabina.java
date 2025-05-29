4d32c9393d075ac8e2dd2f5444591241b9ae05de
saraounifi <s.ounifi@studenti.unibg.it>
2025-05-29 17:54:09 +0200

    Update Cabina.java
    javadoc

--- src/tessere/Cabina.java
+++ src/tessere/Cabina.java
@@ -1,10 +1,22 @@
 package tessere;
 
+/**
+*La classe Cabina rappresenta una tessera contenente membri dell'equipaggio e alieni.
+*Estende la classe e aggiunge attributi per controllare l'equipaggio e gli alieni.
+*/
 public class Cabina extends Tessera {
 	//attributi
 	private int equipaggio;
 	private int alieni;
-	
+
+/**
+*Costruttore della classe cabina.
+*Inizializza i connettori e imposta a zero l'equipaggio e alieni.
+*@param LatoDX   connettore sul lato destro
+*@param LatoSX   connettore sul lato sinistro
+*@param LatoSup  connettore sul lato superiore
+*@param LatoDown connettore sul lato inferiore
+*/	
 	//costruttore
 	public Cabina(Connettore latoDx, Connettore latoSx, Connettore latoSup, Connettore latoDown) {
 		super(latoDx, latoSx, latoSup, latoDown);
@@ -12,23 +24,44 @@ public class Cabina extends Tessera {
 		this.setAlieni(0);
 	}
 
+/**
+* Restituisce il numero dei membri dell'equipaggio nella cabina.
+*@return numero membri equipaggio.
+*/
 	//getter e setter
 	public int getEquipaggio() {
 		return equipaggio;
 	}
 
+/**
+*Imposta il numero dei membri dell'equipaggio nella cabina.
+*@param equipaggio il numero membri equipaggio.
+*/
 	public void setEquipaggio(int equipaggio) {
 		this.equipaggio = equipaggio;
 	}
 
+/**
+* Restituisce il numero alieni nella cabina.
+*@return numero alieni.
+*/
 	public int getAlieni() {
 		return alieni;
 	}
 
+/**
+*Imposta il numero degli alieni nella cabina.
+*@param alieni il numero membri equipaggio.
+*/
 	public void setAlieni(int alieni) {
 		this.alieni = alieni;
 	}
 	//Methodi
+	
+/**
+*Da il nome breve della cabina.
+*@return la stringa CABIN.
+*/
 	@Override
 	public String getNomeBreve() {
     	return " CABIN ";
