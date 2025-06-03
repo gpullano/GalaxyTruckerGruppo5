40b877b5703d98ad23995650b4098e269e7aed5e
saraounifi <s.ounifi@studenti.unibg.it>
2025-06-03 19:05:10 +0200

    Update Provenienza.java
    jd

--- src/carteAvventura/Provenienza.java
+++ src/carteAvventura/Provenienza.java
@@ -1,12 +1,20 @@
 package carteAvventura;
 import java.util.Random;
+
+/**
+*enum che descrive la direzione della carta
+*/
 public enum Provenienza {
 	// attributi 
 	SOPRA, SOTTO, DESTRA, SINISTRA;
+/**generatore di direzioni casuali*/	
 	static Random rand=new Random();
 	
 	//metodi 
 	// generatore casuale 
+/** genera casualmente una delle direzioni di provenienza
+*@return direzione scelta casualmente
+*/
 	public static Provenienza generaProvenienza() {
 		return values()[rand.nextInt(values().length)]; // prendo casualmente o sopra sotto destra o sinistra 
 	}
