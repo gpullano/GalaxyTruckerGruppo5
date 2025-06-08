40c4341631ed8c9eb696a8ce6485eb9f0bd28a2c
ValerioTolotti <v.tolotti@studenti.unibg.it>
2025-06-08 11:53:39 +0200

    Ho aggiunto la funzione attiva della carta 'PolvereStellare'

--- src/carteAvventura/PolvereStellare.java
+++ src/carteAvventura/PolvereStellare.java
@@ -1,7 +1,8 @@
 package carteAvventura;
 
 import java.util.List;
-
+import plance.GestorePlanceNave;
+import plance.PlanceNaveLivello1;
 import gameLogic.ConsoleIO;
 import gameLogic.Giocatore;
 import plance.PlanceVolo;
@@ -28,10 +29,17 @@ public class PolvereStellare extends CartaPerditaGiorniVolo {
 	@Override
 	public void attiva(List<Giocatore> giocatore, PlanceVolo planceVolo, ConsoleIO inputOutput) {
 		// contare i connettori esposti per ogni nave di ogni giocatore 
-		// se il giocatore ne ha, arretrare quanti connettori esposti ha 
-		// planceVolo.
-		
-		
+		// uso un ciclo for 
+		for (int i=0;i<giocatore.size();i++) {
+			Giocatore giocatoreAttuale=giocatore.get(i);
+			PlanceNaveLivello1 planceGiocatore=giocatoreAttuale.getPlanceNave();
+			int connettoriEsposti=GestorePlanceNave.contaConnettoriEsposti(planceGiocatore);
+			// se il giocatore ne ha, arretrare quanti connettori esposti ha 
+			if (connettoriEsposti>0) {
+				// tolgo i gg di volo 
+				planceVolo.getPosizioneGiocatori()[i].aggiornaPosizione(-connettoriEsposti);
+			}
+		}
 	}
 
 
